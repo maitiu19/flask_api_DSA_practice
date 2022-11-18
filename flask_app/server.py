2d3af421cc126f6a2041907c7b7436ce47e8b431
Matthew Knocker <mknocker@cree.com>
2022-11-18 16:14:57 +0000

    added create user route function

--- flask_app/server.py
+++ flask_app/server.py
@@ -45,7 +45,18 @@ class BlogPost(db.Model):
 #create routes
 @app.route("/user", methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json()
+    new_user = User(
+        name=data['name'],
+        email=data['email'],
+        address=data['address'],
+        phone=data['phone']
+        )
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message" : "User created"}), 200
+
 
 @app.route("/user/ascending_id", methods=['GET'])
 def get_all_users_asc():
