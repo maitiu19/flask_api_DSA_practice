839a11f5c4cd67f55e319f38bbe8bdf1e3bec2e8
Matthew Knocker <mknocker@cree.com>
2022-11-21 15:59:47 +0000

    added test

--- flask_app/test_data.py
+++ flask_app/test_data.py
@@ -1,4 +1,17 @@
 from app import User
+from linked_list import LinkedList
+from flask import jsonify
 
-query_user = User.query.all()
-print(query_user)
+all_users = User.query.all()
+all_users_ll = LinkedList()
+
+for user in all_users:
+    all_users_ll.insert_beginning(
+        {
+            "id" : user.id,
+            "name" : user.name
+        }
+    )
+
+
+print((all_users_ll.to_list()))
